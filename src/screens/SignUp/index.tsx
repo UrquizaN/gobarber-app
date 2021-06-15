cce9a7d44368721a271abc4b72f3062b33f4c78c
Urquiza <urquiza-nobrega@outlook.com>
2021-06-14 21:18:31 -0300

    signup api integration

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -42,9 +43,11 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
       Alert.alert('Cadastro realizado com sucesso!', 'Você já pode realizar logon no GoBarber.');
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
